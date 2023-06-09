4dbcc79006f101ffc00cd8c2ff0c838378f6306c
rushemaa <rushemaa@gmail.com>
2023-06-09 12:50:00 +0200

    Query and Update animals tables

--- queries.sql
+++ queries.sql
@@ -8,3 +8,34 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT *  FROM animals WHERE neutered=TRUE;
 SELECT *  FROM animals WHERE name !='Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+BEGIN;
+UPDATE animals set species ='unspecified';
+ROLLBACK;
+
+BEGIN; 
+UPDATE animals set species ='digimon' WHERE name like '%mon';
+UPDATE animals set species ='pokemon' WHERE species IS NULL;
+COMMIT;
+
+BEGIN; 
+DELETE FROM animals;
+ROLLBACK;
+
+BEGIN; 
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT born_after_2022; 
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK to born_after_2022;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+
+SELECT COUNT(*) as number_of_animals FROM animals;
+SELECT COUNT(*) as never_tried_escape FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) AS average_weight FROM animals;
+SELECT name FROM animals WHERE escape_attempts = (SELECT MAX(escape_attempts) FROM animals);
+SELECT name FROM animals WHERE neutered = TRUE;
+SELECT name FROM animals WHERE neutered = FALSE;
+SELECT species,MAX(weight_kg) as max_weight, MIN(weight_kg) AS min_weight FROM animals GROUP BY species;
+SELECT species,AVG(escape_attempts) as avg_escape_attempts FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
+
